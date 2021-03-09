cdbdb83f352d64b0db6d50fd6b453b566063c0e5
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-09 13:42:03 +0300

    The greedy banker's commission.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
+import javax.sound.midi.MetaMessage
+
+const val minAmount: Int = 3500
 fun main() {
-    
+    print("Введите сумму перевода в копейках: ")
+    val amount:Int = readLine()!!.toInt()
+    printMyAmountСommission(amount)
+}
+
+fun printMyAmountСommission(amount:Int) {
+    val amountRub:Int = Math.floor((amount/100).toDouble()).toInt()
+    val amountKop:Int = amount-amountRub*100
+    val commission:Double = (amount*0.0075).toDouble()
+    if (commission<=minAmount) {
+        println("""Сумма перевода: $amountRub руб. $amountKop коп.
+      Комиссия: ${Math.floor((minAmount/100).toDouble()).toInt()} руб.
+        """.trimMargin())
+    } else {
+//  Комиссия жадного банкира >:)) с суммы 4666 руб 67 коп комиссия больше >:))
+        println("""Сумма перевода: $amountRub руб. $amountKop коп.
+      Комиссия: ${Math.floor((commission/100)).toInt()} руб. ${commission-Math.floor((commission/100)).toInt()*100} коп.
+        """.trimMargin())
+    }
 }
